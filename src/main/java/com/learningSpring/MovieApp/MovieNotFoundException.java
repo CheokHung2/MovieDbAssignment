11a7a91455174a5e7c064125216401ffce4e91d4
DESKTOP-PJ6F77F\Cheok Hung Soong <cheokhung.soong@softspace.com.my>
2023-03-06 14:14:44 +0800

    temporarily removed security. Reworks Actors list into a separate table

--- src/main/java/com/learningSpring/MovieApp/MovieNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.learningSpring.MovieApp;
-
-public class MovieNotFoundException extends RuntimeException{
-    public MovieNotFoundException(Long id) {
-        super("could not find movie with id: ");
-    }
-}
--- src/main/java/com/learningSpring/MovieApp/MovieRepository.java
+++
@@ -1,6 +0,0 @@
-package com.learningSpring.MovieApp;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MovieRepository extends JpaRepository<Movie,Long> {
-}
